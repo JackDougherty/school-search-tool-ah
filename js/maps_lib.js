3e98b2d54c444fa067273b3059f04c95a21a156b
Jack Dougherty <jack.dougherty@trincoll.edu>
2015-02-27 10:41:43 -0500

    Update maps_lib.js

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -26,8 +26,8 @@ var MapsLib = {
 
   //MODIFY the encrypted Table IDs of your Fusion Tables (found under File => About)
   //NOTE: numeric IDs will be depricated soon
-  fusionTableId:      "14fD7gjYmVq-7ndCt7sa1-N6Ft9urfmQT-jhMI6gs", //Point data layer
-  // Using JackCopy of SmarterHartford table https://www.google.com/fusiontables/DataSource?docid=14fD7gjYmVq-7ndCt7sa1-N6Ft9urfmQT-jhMI6gs
+  fusionTableId:      "1u1KUXrwtoZWmYJrno7NdwyyJ_lWSwrnw4L8ftBsp", //Point data layer
+  // Using SmarterHartford table https://www.google.com/fusiontables/DataSource?docid=1u1KUXrwtoZWmYJrno7NdwyyJ_lWSwrnw4L8ftBsp
 
   polygon1TableID:    "1o-Xs4sG6qgc5u_MLwzVwjOX14WUul4oA1QlZTpTx", //HPS zones
 
